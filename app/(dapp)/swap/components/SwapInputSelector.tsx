301dc066cd82c542871f910d193bc00d38c4a4da
AL <dreamwrapper@protonmail.com>
2024-05-01 02:28:11 +0700

    remove unofficial wocta

--- app/(dapp)/swap/components/SwapInputSelector.tsx
+++ app/(dapp)/swap/components/SwapInputSelector.tsx
@@ -1,6 +1,14 @@
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Select, SelectContent, SelectItem, SelectScrollDownButton, SelectScrollUpButton, SelectTrigger, SelectValue } from '@/components/ui/select'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 import { cn, formatStringNumber } from '@/lib/utils'
 import { NATIVE_OCTA, matchToken, tokenList } from '@/tokens/tokenList'
 import Image from 'next/image'
@@ -31,18 +39,38 @@ function Balance({ currency }: { currency: string }) {
     },
   })
 
-  const balance = currency === NATIVE_OCTA.symbol ? octaBalance?.value ?? BigInt(0) : tokenBalance ?? BigInt(0)
+  const balance =
+    currency === NATIVE_OCTA.symbol
+      ? octaBalance?.value ?? BigInt(0)
+      : tokenBalance ?? BigInt(0)
 
   const formatBalance = formatStringNumber(formatEther(balance), true)
 
-  return <p className={cn('invisible text-sm text-muted-foreground', !isNaN(Number(formatBalance)) && 'visible')}>Balance : {formatBalance}</p>
+  return (
+    <p
+      className={cn(
+        'invisible text-sm text-muted-foreground',
+        !isNaN(Number(formatBalance)) && 'visible'
+      )}
+    >
+      Balance : {formatBalance}
+    </p>
+  )
 }
 
 function Price() {
   return <p className='invisible text-sm text-muted-foreground'>$0 </p>
 }
 
-function TokenSelector({ currency, onSetToken, disabledToken }: { currency: string; onSetToken: Dispatch<SetStateAction<string>>; disabledToken: string }) {
+function TokenSelector({
+  currency,
+  onSetToken,
+  disabledToken,
+}: {
+  currency: string
+  onSetToken: Dispatch<SetStateAction<string>>
+  disabledToken: string
+}) {
   const getTokenList = tokenList()
   const currentToken = getTokenList.find((token) => token.symbol === currency)
 
@@ -51,20 +79,42 @@ function TokenSelector({ currency, onSetToken, disabledToken }: { currency: stri
       <SelectTrigger className='basis-3/4 bg-background sm:basis-1/2'>
         <SelectValue aria-label={currency}>
           <span className='flex items-center gap-x-2'>
-            <Image src={currentToken?.logoURI as string} alt={`${currentToken?.symbol} Logo`} width={22} height={22} />
-            <span className='text-lg font-semibold'>{currentToken?.symbol}</span>
+            <Image
+              src={currentToken?.logoURI as string}
+              alt={`${currentToken?.symbol} Logo`}
+              width={22}
+              height={22}
+            />
+            <span className='text-lg font-semibold'>
+              {currentToken?.symbol}
+            </span>
           </span>
         </SelectValue>
       </SelectTrigger>
       <SelectContent className='max-h-[13rem]'>
-        {getTokenList.map((token) => (
-          <SelectItem value={token.symbol} key={token.symbol} disabled={token.symbol === disabledToken}>
-            <span className='flex items-center gap-x-2'>
-              <Image src={token.logoURI} alt={`${token.symbol} Logo`} width={22} height={22} />
-              <span className='text-lg font-semibold'>{token.symbol}</span>
-            </span>
-          </SelectItem>
-        ))}
+        {getTokenList.map((token) => {
+          if (token.symbol === 'WOCTA') {
+            return null
+          }
+
+          return (
+            <SelectItem
+              value={token.symbol}
+              key={token.symbol}
+              disabled={token.symbol === disabledToken}
+            >
+              <span className='flex items-center gap-x-2'>
+                <Image
+                  src={token.logoURI}
+                  alt={`${token.symbol} Logo`}
+                  width={22}
+                  height={22}
+                />
+                <span className='text-lg font-semibold'>{token.symbol}</span>
+              </span>
+            </SelectItem>
+          )
+        })}
       </SelectContent>
     </Select>
   )
@@ -93,7 +143,11 @@ export default function SwapInputSelector({
           placeholder='0'
           className='border-none text-2xl shadow-none placeholder-shown:text-2xl focus-visible:ring-0 focus-visible:ring-offset-0'
         />
-        <TokenSelector currency={currency} onSetToken={onSetToken} disabledToken={disabledToken} />
+        <TokenSelector
+          currency={currency}
+          onSetToken={onSetToken}
+          disabledToken={disabledToken}
+        />
       </div>
       <div className='mt-5 flex items-center justify-between px-2'>
         <Price />
